93d65f7a6dc9269eecc7e2238c5e60c014cbe110
Xavier <xthom001@gmail.com>
2015-09-18 11:39:03 -0500

    removed image compression

--- gulpfile.js
+++ gulpfile.js
@@ -32,11 +32,6 @@
     // Gulp image compression task
     gulp.task('images', function() {
         return gulp.src('img/**')
-            .pipe(imagemin({
-                optimizationLevel: 5,
-                progressive: true,
-                interlaced: true
-            }))
             .pipe(gulp.dest('public/img'))
     });
 
